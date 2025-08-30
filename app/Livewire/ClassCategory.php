7017cbe37337e56671f9341071b8077d0eb55a7a
Saloni Gupta <salonigupta01230@gmail.com>
2025-08-30 15:45:34 +0530

    Add initial models, migration, and Livewire component for class categories

---
+++ app/Livewire/ClassCategory.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ClassCategory extends Component
+{
+    public function render()
+    {
+        return view('livewire.class-category');
+    }
+}
