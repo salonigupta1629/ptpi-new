100e1a96e74022e7f491b85236293152f1299954
sadique-cws <sadique.hussain96@gmail.com>
2025-09-01 14:41:47 +0530

    feat: Update login layout to use auth template and add registration page

--- app/Livewire/Landing/Register.php
+++ app/Livewire/Landing/Register.php
@@ -4,10 +4,10 @@ namespace App\Livewire\Landing;
 
 use Livewire\Component;
 
-class Login extends Component
+class Register extends Component
 {
     public function render()
     {
-        return view('livewire.landing.login')->layout("layouts.auth");
+        return view('livewire.landing.register');
     }
 }
