3529191e126e081301677676209f4cb67bae7992
theaman82 <amankumaraman55555@gmail.com>
2025-09-06 11:54:33 +0530

    job details section work

--- app/Livewire/Teacher/JobDetails.php
+++ app/Livewire/Teacher/JobDetails.php
@@ -2,11 +2,35 @@
 
 namespace App\Livewire\Teacher;
 
+use App\Models\Skill;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class JobDetails extends Component
 {
+    // public $searchSkill = '';
+    // public $skills = [];
+    // public $selectedSkill = null;
+    public $currentlyWorking = false;
+
+     public $query = '';
+    public $skills = [];
+    public $selectedSkill = null;
+
+    public function updatedQuery()
+    {
+        $this->skills = Skill::where('name', 'like', '%' . $this->query . '%')
+            ->limit(5)
+            ->get();
+    }
+
+    public function selectSkill($id)
+    {
+        $this->selectedSkill = Skill::find($id);
+        $this->query = $this->selectedSkill->name;
+        $this->skills = []; // clear dropdown
+    }
+
     #[Layout('layouts.teacher')]
     public function render()
     {
